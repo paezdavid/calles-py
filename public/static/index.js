787ffe6395cefdbc187e754eecc78ff5458d4249
paezdavid <davidgithub1@gmail.com>
2023-03-10 20:10:20 -0300

    added comments

--- public/static/index.js
+++ public/static/index.js
@@ -1,20 +1,15 @@
-console.log("debug")
 
 let map = L.map('map').setView([-25.321124, -57.551063], 12);
-
+const coords_index = document.querySelector(".coords_index") // The hidden input where the data is stored
+const coords_obj = JSON.parse(coords_index.getAttribute("value")) // Convert the JSON that came from the client into an object
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-
-const coords_index = document.querySelector(".coords_index")
-const coords_JSON = JSON.parse(coords_index.getAttribute("value"))
-
-console.log(coords_JSON)
-
-coords_JSON.arr_of_baches.forEach(bache_coord => {
+// Iterate over the array of baches that's inside the coords_obj
+coords_obj.arr_of_baches.forEach(bache_coord => {
     const marker = L.marker([bache_coord.street_coords.lat, bache_coord.street_coords.lng]).addTo(map);
 
     function validate_category(category) {
@@ -29,12 +24,30 @@ coords_JSON.arr_of_baches.forEach(bache_coord => {
         }
     }
 
+    const upload_date = new Date(bache_coord.upload_date)
+
     marker.bindPopup(`
-        <img src="${bache_coord.image_url}" style="margin: 0 auto; max-width: 150px;" />
+        <img src="${bache_coord.image_url}" style="margin: 0 auto; max-width: 150px; margin-bottom: 0.5rem" />
         \n
-        ${ bache_coord.opt_address ? `Dirección: ${bache_coord.opt_address}` : "" }
+
+        <h3 style="font-size: 1.125rem; font-weight: bold;">${validate_category(bache_coord.street_category)}</h3>
         \n
-        ${validate_category(bache_coord.street_category)}
+
+        ${ bache_coord.opt_address ? 
+            `<span style="font-weight: bold; margin: 0.2rem 0;">Dirección:</span> ${bache_coord.opt_address}` : 
+            "" 
+        }
+        \n
+
+        ${ bache_coord.opt_user_comment ? 
+            `<p style="font-weight: bold; margin: 0.2rem 0; text-decoration: underline;">Comentario del usuario:</p>
+            <p style="margin: 0.2rem 0;">${bache_coord.opt_user_comment}</p>` : 
+            "" 
+        }
+        \n
+
+        <p style="text-align:right;">${ upload_date.toLocaleString('py-ES') }</p>
+        
     
-    `, { minWidth: 200, maxWidth: 250, className: "popup_img" }).openPopup()
+    `, { minWidth: 200, maxWidth: 250 }).openPopup()
 });

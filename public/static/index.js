762d6a27a58f8a2bf32e679eee28ea8c4a8daeca
paezdavid <davidgithub1@gmail.com>
2023-04-27 21:24:30 -0400

    added API data fetching

--- public/static/index.js
+++ public/static/index.js
@@ -1,53 +1,62 @@
 
 let map = L.map('map').setView([-25.321124, -57.551063], 12);
-const coords_index = document.querySelector(".coords_index") // The hidden input where the data is stored
-const coords_obj = JSON.parse(coords_index.getAttribute("value")) // Convert the JSON that came from the client into an object
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-// Iterate over the array of baches that's inside the coords_obj
-coords_obj.arr_of_baches.forEach(bache_coord => {
-    const marker = L.marker([bache_coord.street_coords.lat, bache_coord.street_coords.lng]).addTo(map);
 
-    function validate_category(category) {
-        if (category === "bache") {
-            return "Bache"
-        } else if (category === "basural") {
-            return "Basural"
-        } else if (category === "perdida_de_agua") {
-            return "Pérdida de Agua"
-        } else if (category === "vereda_mal_estado") {
-            return "Vereda en mal estado"
-        }
-    }
+// We call the API that returns all the data we need to populate the main map
+fetch('http://127.0.0.1:8000')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data.denuncias[0])
 
-    const upload_date = new Date(bache_coord.upload_date)
 
-    marker.bindPopup(`
-        <img src="${bache_coord.image_url}" style="margin: 0 auto; max-width: 150px; margin-bottom: 0.5rem" />
-        \n
-
-        <h3 style="font-size: 1.125rem; font-weight: bold;">${validate_category(bache_coord.street_category)}</h3>
-        \n
-
-        ${ bache_coord.opt_address ? 
-            `<span style="font-weight: bold; margin: 0.2rem 0;">Dirección:</span> ${bache_coord.opt_address}` : 
-            "" 
-        }
-        \n
-
-        ${ bache_coord.opt_user_comment ? 
-            `<p style="font-weight: bold; margin: 0.2rem 0; text-decoration: underline;">Comentario del usuario:</p>
-            <p style="margin: 0.2rem 0;">${bache_coord.opt_user_comment}</p>` : 
-            "" 
-        }
-        \n
-
-        <p style="text-align:right;">${ upload_date.toLocaleString('py-ES') }</p>
-        
+    data.denuncias.forEach(denuncia => {
+        const marker = L.marker([denuncia.street_coords.lat, denuncia.street_coords.lng]).addTo(map);
     
-    `, { minWidth: 200, maxWidth: 250 }).openPopup()
-});
+        function validate_category(category) {
+            if (category === "bache") {
+                return "Bache"
+            } else if (category === "basural") {
+                return "Basural"
+            } else if (category === "perdida_de_agua") {
+                return "Pérdida de Agua"
+            } else if (category === "vereda_mal_estado") {
+                return "Vereda en mal estado"
+            }
+        }
+    
+        const upload_date = new Date(denuncia.upload_date)
+    
+        marker.bindPopup(`
+            <img src="${denuncia.image_url}" style="margin: 0 auto; max-width: 150px; margin-bottom: 0.5rem" />
+            \n
+    
+            <h3 style="font-size: 1.125rem; font-weight: bold;">${validate_category(denuncia.street_category)}</h3>
+            \n
+    
+            ${ denuncia.opt_address ? 
+                `<span style="font-weight: bold; margin: 0.2rem 0;">Dirección:</span> ${denuncia.opt_address}` : 
+                "" 
+            }
+            \n
+    
+            ${ denuncia.opt_user_comment ? 
+                `<p style="font-weight: bold; margin: 0.2rem 0; text-decoration: underline;">Comentario del usuario:</p>
+                <p style="margin: 0.2rem 0;">${denuncia.opt_user_comment}</p>` : 
+                "" 
+            }
+            \n
+    
+            <p style="text-align:right;">${ upload_date.toLocaleString('py-ES') }</p>
+            
+        
+        `, { minWidth: 200, maxWidth: 250 }).openPopup()
+    });
+
+  })
+  .catch(error => console.error(error));
+

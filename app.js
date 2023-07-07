2ec688a3d27db2d2985bfd337c803e92ee5d5029
paezdavid <davidgithub1@gmail.com>
2023-07-07 08:38:33 -0400

    added a new route handler

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const formRouter = require("./routes/formRouter")
 app.use("/form", formRouter)
 const indexRouter = require("./routes/indexRouter")
 app.use("/", indexRouter)
+const actualizarRouter = require("./routes/actualizarRouter")
+app.use("/actualizar", actualizarRouter)
 
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "pug")
@@ -26,6 +28,10 @@ app.get("/", (req, res) => {
 app.get("/form", (req, res) => {
     res.render("form")
 })  
+
+app.get("/actualizar", (req, res) => {
+    res.render("actualizar")
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
